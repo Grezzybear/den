956d75c1da2850f87f04043de78b3aff31820fbc
David <41884971+Grezzybear@users.noreply.github.com>
2024-04-26 00:33:23 -0400

    Update setup.sh
    jellyseerr spelled wrong (was jellyseer)
    forgot to put echo for my env values like a dummy

--- media-server/setup.sh
+++ media-server/setup.sh
@@ -5,7 +5,7 @@ sudo useradd proton -u 10001
 sudo useradd qbittorrent -u 10002
 sudo useradd jellyfin -u 10003
 sudo useradd audiobookshelf -u 10004
-sudo useradd jellyseer -u 10005
+sudo useradd jellyseerr -u 10005
 sudo useradd prowlarr -u 10006
 sudo useradd flaresolverr -u 10007
 sudo useradd sonarr -u 10008
@@ -20,7 +20,7 @@ sudo usermod -a -G mediacenter proton
 sudo usermod -a -G mediacenter qbittorrent
 sudo usermod -a -G mediacenter jellyfin
 sudo usermod -a -G mediacenter audiobookshelf
-sudo usermod -a -G mediacenter jellyseer
+sudo usermod -a -G mediacenter jellyseerr
 sudo usermod -a -G mediacenter prowlarr
 sudo usermod -a -G mediacenter flaresolverr
 sudo usermod -a -G mediacenter sonarr
@@ -42,7 +42,7 @@ sudo chown -R proton:mediacenter config/proton-config
 sudo chown -R qbittorrent:mediacenter config/qbittorrent-config
 sudo chown -R jellyfin:mediacenter config/jellyfin-config
 sudo chown -R audiobookshelf:mediacenter config/audiobookshelf-config
-sudo chown -R jellyseer:mediacenter config/jellyseer-config
+sudo chown -R jellyseer:mediacenter config/jellyseerr-config
 sudo chown -R prowlarr:mediacenter config/prowlarr-config
 sudo chown -R flaresolverr:mediacenter config/flaresolverr-config
 sudo chown -R sonarr:mediacenter config/sonarr-config
@@ -56,19 +56,19 @@ sudo chown -R freshrss:mediacenter config/freshrss-config
 # set our base env values
 echo "UID=$(id -u)" >> .env
 echo "BASE=$(pwd)" >> .env
-"PROTON-UID=10001" >> .env
-"QBIT-UID=10002" >> .env
-"JELLYFIN-UID=10003" >> .env
-"AUDIOBOOK-UID=10004" >> .env
-"SEER-UID=10005" >> .env
-"PROWLARR-UID=10006" >> .env
-"FLARESOLVERR-UID=10007" >> .env
-"SONARR-UID=10008" >> .env
-"RADARR-UID=10009" >> .env
-"READARR-UID=10010" >> .env
-"LIDARR-UID=10011" >> .env
-"BAZARR-UID=10012" >> .env
-"LAZYLIBRARIAN-UID=10013" >> .env
-"FRESHRSS-UID=10014" >> .env
-"MEDIA-GID=10000" >> .env
-"TZ=Etc/UTC" >> .env
+echo "PROTON-UID=10001" >> .env
+echo "QBIT-UID=10002" >> .env
+echo "JELLYFIN-UID=10003" >> .env
+echo "AUDIOBOOK-UID=10004" >> .env
+echo "SEER-UID=10005" >> .env
+echo "PROWLARR-UID=10006" >> .env
+echo "FLARESOLVERR-UID=10007" >> .env
+echo "SONARR-UID=10008" >> .env
+echo "RADARR-UID=10009" >> .env
+echo "READARR-UID=10010" >> .env
+echo "LIDARR-UID=10011" >> .env
+echo "BAZARR-UID=10012" >> .env
+echo "LAZYLIBRARIAN-UID=10013" >> .env
+echo "FRESHRSS-UID=10014" >> .env
+echo "MEDIA-GID=10000" >> .env
+echo "TZ=Etc/UTC" >> .env
