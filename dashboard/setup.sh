c8da9deceb770493dfd55bc866dc4a2b4233665a
David <41884971+Grezzybear@users.noreply.github.com>
2024-05-06 14:19:02 -0400

    Update setup.sh
    Forgot a couple echo commands

--- dashboard/setup.sh
+++ dashboard/setup.sh
@@ -14,6 +14,6 @@ sudo chown -R homepage:dashboard config/homepage-config
 # set our base env values
 echo "UID=$(id -u)" >> .env
 echo "BASE=$(pwd)" >> .env
-"HOMEPAGE-UID=12001" >> .env
-"DASHBOARD-GID=12000" >> .env
-"TZ=Etc/UTC" >> .env
+echo "HOMEPAGE-UID=12001" >> .env
+echo "DASHBOARD-GID=12000" >> .env
+echo "TZ=Etc/UTC" >> .env
