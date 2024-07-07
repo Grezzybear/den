bb1df4ed502ef2955a6f38c455dde73b2a5da5dd
David <41884971+Grezzybear@users.noreply.github.com>
2024-07-06 20:52:56 -0400

    Create setup.sh
    Initial setup for homeassistant

---
+++ home/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Make users and group
+sudo useradd homeassistant -u 15001
+sudo groupadd home -g 15000
+sudo usermod -a -G mediacenter homeassistant
+
+# Make directories
+sudo mkdir -pv data/
+
+# Set permissions
+sudo chmod -R homeassistant:home data/
+
+# set our base env values
+echo "BASE=$(pwd)" >> .env
+echo "TZ=Etc/UTC" >> .env
