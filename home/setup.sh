4db71ac7b9246a050e1fa7bea0fcc1f8ffea667d
David <41884971+Grezzybear@users.noreply.github.com>
2024-07-06 21:46:46 -0400

    Update setup.sh
    Added support for esphome

--- home/setup.sh
+++ home/setup.sh
@@ -2,14 +2,17 @@
 
 # Make users and group
 sudo useradd homeassistant -u 15001
+sudo useradd esphome -u 15002
 sudo groupadd home -g 15000
-sudo usermod -a -G mediacenter homeassistant
+sudo usermod -a -G home homeassistant
+sudo usermod -a -G home esphome
 
 # Make directories
-sudo mkdir -pv data/
+sudo mkdir -pv config/{homeassistant,esphome}-config
 
 # Set permissions
-sudo chown -R homeassistant:home data/
+sudo chown -R homeassistant:home config/homeassistant-config
+sudo chown -R esphome:home config/esphome-config
 
 # set our base env values
 echo "BASE=$(pwd)" >> .env
