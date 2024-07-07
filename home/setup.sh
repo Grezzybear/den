5d25314f6b69cedb398ef5b42a49376f114ac1a7
David <41884971+Grezzybear@users.noreply.github.com>
2024-07-06 22:29:04 -0400

    Update setup.sh
    Added nginxhome to the mix

--- home/setup.sh
+++ home/setup.sh
@@ -3,16 +3,21 @@
 # Make users and group
 sudo useradd homeassistant -u 15001
 sudo useradd esphome -u 15002
+sudo useradd nginxhome -u 15003
 sudo groupadd home -g 15000
 sudo usermod -a -G home homeassistant
 sudo usermod -a -G home esphome
+sudo usermod -a -G home nginxhome
 
 # Make directories
-sudo mkdir -pv config/{homeassistant,esphome}-config
+sudo mkdir -pv config/{homeassistant,esphome,nginxhome}-config
+sudo mkdir -pv config/letsencrypt
 
 # Set permissions
 sudo chown -R homeassistant:home config/homeassistant-config
 sudo chown -R esphome:home config/esphome-config
+sudo chown -R nginxhome:home config/nginxhome-config
+sudo chown -R nginxhome:home config/letsencrypt
 
 # set our base env values
 echo "BASE=$(pwd)" >> .env
