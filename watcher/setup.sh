235944865bb02215b6a62482af2befeff09436ba
David <41884971+Grezzybear@users.noreply.github.com>
2024-07-15 00:51:51 -0400

    Update setup.sh
    Added adguard home

--- watcher/setup.sh
+++ watcher/setup.sh
@@ -6,6 +6,7 @@ sudo useradd watchtower -u 16002
 sudo useradd prometheus -u 16003
 sudo useradd grafana -u 16004
 sudo useradd uptime-kuma -u 16005
+sudo useradd adguardhome -u 16006
 sudo groupadd watcher -g 16000
 sudo usermod -a -G watcher dozzle
 sudo usermod -a -G watcher watchtower
@@ -13,16 +14,19 @@ sudo usermod -a -G docker watchtower
 sudo usermod -a -G watcher prometheus
 sudo usermod -a -G watcher grafana
 sudo usermod -a -G watcher uptime-kuma
+sudo usermod -a -G watcher adguardhome
 
 # Make directories
-sudo mkdir -pv {prometheus,grafana,uptime-kuma,dozzle}/{data}
+sudo mkdir -pv {prometheus,grafana,uptime-kuma,dozzle,adguardhome}/{data}
 sudo mkdir -pv prometheus/config
+sudo mkdir -pv adguardhome/config
 
 # Set permissions
 sudo chown -R prometheus:watcher prometheus
 sudo chown -R grafana:watcher grafana
 sudo chown -R uptime-kuma:watcher uptime-kuma
 sudo chown -R dozzle:watcher dozzle
+sudo chown -R adguardhome:watcher adguardhome
 
 # set our base env values
 echo "Dozzle Username:"
