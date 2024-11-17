aef12171385bf5028c3c6a76a912047a6cb44753
David <41884971+Grezzybear@users.noreply.github.com>
2024-11-17 00:39:16 -0500

    Added Mealie setup support - Update setup.sh
    Added mealie and mealie-db folders and env variables

--- home/setup.sh
+++ home/setup.sh
@@ -2,15 +2,27 @@
 
 # Make users and group
 sudo useradd homeassistant -u 15001
+sudo useradd mealie -u 15002
 sudo groupadd home -g 15000
 sudo usermod -a -G home homeassistant
+sudo usermod -a -G home mealie
 
 # Make directories
-sudo mkdir -pv config/{homeassistant,esphome}-config
+sudo mkdir -pv config/{homeassistant,esphome,mealie,mealie-db}-config
 
 # Set permissions
 sudo chown -R homeassistant:home config/homeassistant-config
+sudo chown -R mealie:home config/mealie-config
+sudo chown -R mealie:home config/mealie-db-config
 
 # set our base env values
 echo "BASE=$(pwd)" >> .env
 echo "TZ=Etc/UTC" >> .env
+
+echo "Mealie DB Username:"
+read -r MEALIEUN
+echo "Mealie DB Password:"
+read -rs MEALIEPW
+
+echo "MEALIEDBUN=$MEALIEUN" >> .env
+echo "MEALIEDBPW=$MEALIEPW" >> .env
